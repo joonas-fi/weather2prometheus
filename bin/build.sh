fdc1f0d85e04eaadc29c5f04d6dbb17ed0d6b1bc
Joonas Loppi <joonas+code@joonas.fi>
2023-02-15 11:38:01 +0200

    update build boilerplates, Go, buildkit-golang

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -eu
 
+
+build-go-project.sh --directory=cmd/weather/ --binary-basename=weather
+
+# using this rest for packageLambdaFunction
+
 source /build-common.sh
 
 BINARY_NAME="weather"
 COMPILE_IN_DIRECTORY="cmd/weather"
-
-standardBuildProcess
 
 buildstep packageLambdaFunction
